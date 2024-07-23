c461410aed647ad56abeba2a70549a03bcc83533
ikucuk <incikucuk98@gmail.com>
2024-07-23 11:32:17 +0300

    first-commit

---
+++ src/main/java/com/ikucuk/todo_management_project/TodoManagementProjectApplication.java
@@ -0,0 +1,13 @@
+package com.ikucuk.todo_management_project;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TodoManagementProjectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TodoManagementProjectApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/ikucuk/todo_management_project/TodoManagementProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ikucuk.todo_management_project;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TodoManagementProjectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
